d797c4a066ba2a4fc07fbbfa2a4b5c66bf833f88
Danish Malik <malik083@morris.umn.edu>
2017-10-25 18:50:30 -0500

    Worked on 'EchoClient.java' to accept arguements and generate output.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,15 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+  public static void main(String[] args){
+      InputStream keyboard = System.in;
+
+      Socket socket = new Socket("127.0.0.1", 6013);
+
+      InputStream input = socket.getInputStream();
+      OutputStream output = socket.getOutputStream();
+
+  }
 }
