2746dc839520bfbf4a2386e93c7c39725397b613
Danish Malik <malik083@morris.umn.edu>
2017-10-25 19:15:51 -0500

    Finished working on 'EchoClient.java'. All tests passing.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class EchoClient {
   public static void main(String[] args){
+    try{
       InputStream keyboard = System.in;
 
       Socket socket = new Socket("127.0.0.1", 6013);
@@ -11,5 +12,20 @@ public class EchoClient {
       InputStream input = socket.getInputStream();
       OutputStream output = socket.getOutputStream();
 
+      int a;
+      while ((a = keyboard.read()) != - 1){
+        output.write(a);
+        System.out.write(input.read());
+      }
+
+      keyboard.close();
+      socket.close();
+
+      System.out.flush();
+
+    }catch (IOException ioe){
+      System.out.println("Exception caught");
+      System.err.println(ioe);
   }
+ }
 }
