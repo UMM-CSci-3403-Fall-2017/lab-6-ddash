fc32c818e495418b323e3aa9da4dffa344218640
Danish Malik <malik083@morris.umn.edu>
2017-10-26 18:23:55 -0500

    Finish writing 'EchoServer.java'.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,28 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+  public static void main(String[] args) {
+    try {
+      ServerSocket sock = new ServerSocket(6013);
+
+      while (true) {
+        Socket client = sock.accept();
+
+        FileInputStream reader = (FileInputStream) client.getInputStream();
+        FileOutputStream writer = (FileOutputStream) client.getOutputStream();
+        
+	int a;
+
+        while ((a = reader.read()) != - 1) {
+          writer.write(a);
+        }
+        client.close();
+      }
+    } catch (IOException e) {
+        System.err.println(e);
+    }
+  }
 }
